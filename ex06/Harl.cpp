a406185994b56b02708e896c816d73511e50e545
Mohamed Bousoufi <mbousouf@e3r9p11.1337.ma>
2023-09-15 20:07:59 +0100

    still Makefile

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -5,7 +5,7 @@ void Harl :: debug( void )
 	std :: cout << "[ DEBUG ]" << std :: endl;
 	std :: cout << "I love having extra bacon for my";
 	std :: cout <<"7XL-double-cheese-triple-pickle-specialketchup burger";
-	std :: cout <<". I really do!" << std :: endl;
+	std :: cout <<". I really do!" << std :: endl<< std :: endl;
 }
 
 void Harl :: info( void )
@@ -14,19 +14,20 @@ void Harl :: info( void )
 	std :: cout << "I cannot believe adding extra bacon costs more money. You didn’t put";
 	std :: cout << "enough bacon in my burger! If you did, I wouldn’t be asking for more!";
 	std :: cout << std ::endl;
+	std :: cout << std ::endl;
 }
 
 void Harl :: warning( void )
 {
 	std :: cout << "[ WARNING ]" << std :: endl;
 	std :: cout << "I think I deserve to have some extra bacon for free. I’ve been coming for";
-	std :: cout << "years whereas you started working here since last month." << std :: endl;
+	std :: cout << "years whereas you started working here since last month." << std :: endl<< std :: endl;
 }
 
 void Harl :: error( void )
 {
 	std :: cout << "[ ERROR ]" << std :: endl;
-	std :: cout << "This is unacceptable! I want to speak to the manager now." << std :: endl;
+	std :: cout << "This is unacceptable! I want to speak to the manager now." << std :: endl<< std :: endl;
 }
 
  void Harl :: complain(std :: string level)
